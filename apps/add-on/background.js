ad4f9ccfe0ff554a8331fed991b5535d433d2581
n0psledbyte <n0psledbyte@gmail.com>
2018-07-21 08:24:39 +0700

    add addons apps

---
+++ apps/add-on/background.js
@@ -0,0 +1,33 @@
+console.log("Webcomplete addons starting..");
+var port = browser.runtime.connectNative("ping_pong");
+console.log(port);
+
+/*
+Listen for messages from the app.
+*/
+port.onMessage.addListener((response) => {
+  console.log("Received: " + response);
+});
+
+/*
+On a click on the browser action, send the app a message.
+*/
+
+function onExecuted(result) {
+  port.postMessage(result);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+var cmd = "[...new Set(document.body.innerText.match(/\\w+/g))].sort().join('\\n');";
+
+browser.tabs.onActivated.addListener((tabId, changeInfo, tabInfo) => {
+  console.log("Sending data to apps");
+  var executing = browser.tabs.executeScript({
+    code: cmd
+  });
+
+  executing.then(onExecuted, onError);
+});
